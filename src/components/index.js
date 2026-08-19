f487c4a8845c0a4890c9356f6607c6fbce570805
Yash Gupta <yashgupta8810855482@gmail.com>
2026-08-20 01:18:35 +0530

    Add index file to export main components for the application

---
+++ src/components/index.js
@@ -0,0 +1,47 @@
+import Header from "./Header/Header";
+
+import Footer from "./Footer/Footer";
+
+import Container from "./container/container";
+
+import Logo from "./Logo";
+
+import LogoutBtn from "./Header/LogoutBtn";
+
+import Input from "./Input";
+
+import Button from "./Button";
+
+import Select from "./Select";
+
+import RTE from "./RTE";
+
+import Signup from "./Signup";
+
+import Login from "./Login";
+
+import PostCard from "./PostCard";
+
+
+import PostForm from "./post-form/PostForm";
+
+import AuthLayout from "./AuthLayout";
+
+
+
+export {
+       Header,
+       Footer,
+       Container,
+       Logo,
+       LogoutBtn,
+       Input,
+       Button,
+       Select,
+       RTE,
+       Signup,
+       Login,
+       PostCard,
+       PostForm,
+       AuthLayout
+    }
